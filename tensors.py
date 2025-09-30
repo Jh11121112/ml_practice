a20466466b853c5bb01d5824bb6327595ca0a5a5
Janhvi <haldwaniajanhvi@gmail.com>
2025-09-30 19:48:03 +0530

    initial commit

---
+++ tensors.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Create a small table (DataFrame)
+data = {
+    "Name": ["Amit", "Neha", "Raj"],
+    "Age": [25, 30, 22],
+    "City": ["Delhi", "Mumbai", "Bangalore"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)   # print the table
