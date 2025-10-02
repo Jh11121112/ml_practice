6d0a65b68c3ba213696f80d88861a1b9505a2c52
Janhvi <haldwaniajanhvi@gmail.com>
2025-10-02 20:34:39 +0530

    update tensors.py formatting

--- tensors.py
+++ tensors.py
@@ -1,12 +1,22 @@
-import pandas as pd
+import numpy as np
+# 0 D tensor/scalar
+a = np.array(4)
+print(a.shape)
+print(a.ndim) # shows no of dimesnions
 
-# Create a small table (DataFrame)
-data = {
-    "Name": ["Amit", "Neha", "Raj"],
-    "Age": [25, 30, 22],
-    "City": ["Delhi", "Mumbai", "Bangalore"]
-}
+# 1D tensor/vector
+b = np.array([1,2,3,4])
+print(b.shape)
+print(b.ndim)
 
-df = pd.DataFrame(data)
+# 2D tensor/matrix
+c = np.array([[1,2,3],[4,5,6]])
+print(c)
+print(c.shape)
+print(c.ndim)
 
-print(df)
+# 3D tensor
+d = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]])
+print(d)
+print(d.shape)
+print(d.ndim)
